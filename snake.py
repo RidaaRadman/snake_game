9130288d33f68eead5f45709f3f48525daf73f46
RidaaRadman <97172137+RidaaRadman@users.noreply.github.com>
2022-01-18 22:43:45 +0300

    final game

--- snake.py
+++ snake.py
@@ -1,91 +1,167 @@
-from _typeshed import Self
+
+# ***************************
+#       Python Snake        *
+# ***************************
 from tkinter import *
-import random 
-#اعداد الواجه اللعبه
-game_width =700
-game_hight =700
-speed =500
-body_parts =3
-space_size=50
-snake_color="#00FF00"
-food_color="#FF0000"
-bacgrounnd_color="black"
+import random
+#تعريف الثوبت او عدادات اللعبة
+GAME_WIDTH = 700
+GAME_HEIGHT = 700
+SPEED = 50
+SPACE_SIZE = 50
+BODY_PARTS = 3
+SNAKE_COLOR = "#00FF00"
+FOOD_COLOR = "#FF0000"
+BACKGROUND_COLOR = "#000000"
 
-class snake:
+
+class Snake:
+
     def __init__(self):
-        self.body_size =body_parts 
-        self.coordinates =[]
-        self.squares =[]
+        self.body_size = BODY_PARTS
+        self.coordinates = []
+        self.squares = []
 
-        for i in random(0, body_parts):
-            self.coordinates.append([0,0])
+        for i in range(0, BODY_PARTS):
+            self.coordinates.append([0, 0])
 
-        for x ,y in self.coordinates:
-            square =Canvas.create_rectangle(x , y ,x + space_size ,y + space_size , fill =snake_color , tag="snake")       
+        for x, y in self.coordinates:
+            square = canvas.create_rectangle(x, y, x + SPACE_SIZE, y + SPACE_SIZE, fill=SNAKE_COLOR, tag="snake")
             self.squares.append(square)
-class food:
-  def __init__(Self):
-   x=random.randint(0, (game_width/space_size)-1)*space_size
-   y=random.randint(0, (game_hight/space_size)-1)*space_size
-
-   Self.coordinates =[x,y]
-   Canvas.create_oval(x , y, x +space_size, y+ space_size, fill=food_color, tag="food" )
-  def next_turn( snake ,food):
-     x,y =snake.coordinates[0]
-     if direction == "up ":
-         y-= space_size
-     elif direction == "down ":
-         y+=space_size
-     elif direction == "right ":
-         x=+space_size 
-     elif direction == "left ":  
-         x-=space_size 
-
-     snake.coordinates.insertn(0 , (x,y))
-     square=Canvas.create_rectangle(x,y , x+space_size , y+space_size , fill=snake_color)
-     snake.square.insert(0 ,square)
-     del snake.coordinates[-1] 
-     Canvas.delete(snake.squares[-1])
-     del snake.squares[-1]
-     window.after(speed ,snake ,food )        
-
-  def dirction( new_diraction):
-   pass
-  def check_collision():
-      pass
-  def game_Over():  
-   pass
-
-window = Tk()
-window.title("snek game")
-window.resizable(False, False)
 
 
-score=0
-direction='down'
+class Food:
 
-Label=Label(window , text="score:{}" .format(score) , font =('console',40))
-Label.pack()
+    def __init__(self):
 
-Canvas=Canvas(window , bg=bacgrounnd_color, height=game_hight , width=game_width)
-Canvas.pack()
+        x = random.randint(0, (GAME_WIDTH / SPACE_SIZE)-1) * SPACE_SIZE
+        y = random.randint(0, (GAME_HEIGHT / SPACE_SIZE) - 1) * SPACE_SIZE
+
+        self.coordinates = [x, y]
+
+        canvas.create_oval(x, y, x + SPACE_SIZE, y + SPACE_SIZE, fill=FOOD_COLOR, tag="food")
+
+
+def next_turn(snake, food):
+
+    x, y = snake.coordinates[0]
+
+    if direction == "up":
+        y -= SPACE_SIZE
+    elif direction == "down":
+        y += SPACE_SIZE
+    elif direction == "left":
+        x -= SPACE_SIZE
+    elif direction == "right":
+        x += SPACE_SIZE
+
+    snake.coordinates.insert(0, (x, y))
+
+    square = canvas.create_rectangle(x, y, x + SPACE_SIZE, y + SPACE_SIZE, fill=SNAKE_COLOR)
+
+    snake.squares.insert(0, square)
+
+    if x == food.coordinates[0] and y == food.coordinates[1]:
+
+        global score
+
+        score += 1
+
+        label.config(text="Score:{}".format(score))
+
+        canvas.delete("food")
+
+        food = Food()
+
+    else:
+
+        del snake.coordinates[-1]
+
+        canvas.delete(snake.squares[-1])
+
+        del snake.squares[-1]
+
+    if check_collisions(snake):
+        game_over()
+
+    else:
+        window.after(SPEED, next_turn, snake, food)
+
+
+def change_direction(new_direction):
+
+    global direction
+#بيقرأ ضغط المستخدم عى لكيبورد
+    if new_direction == 'left':
+        if direction != 'right':
+            direction = new_direction
+    elif new_direction == 'right':
+        if direction != 'left':
+            direction = new_direction
+    elif new_direction == 'up':
+        if direction != 'down':
+            direction = new_direction
+    elif new_direction == 'down':
+        if direction != 'up':
+            direction = new_direction
+
+
+def check_collisions(snake):
+
+    x, y = snake.coordinates[0]
+
+    if x < 0 or x >= GAME_WIDTH:
+        return True
+    elif y < 0 or y >= GAME_HEIGHT:
+        return True
+
+    for body_part in snake.coordinates[1:]:
+        if x == body_part[0] and y == body_part[1]:
+            return True
+
+    return False
+
+
+def game_over():
+
+    canvas.delete(ALL)
+    canvas.create_text(canvas.winfo_width()/2, canvas.winfo_height()/2,
+                       font=('consolas',70), text="GAME OVER", fill="red", tag="gameover")
+
+
+window = Tk() # حزمه للتعامل مع واجهات GUI
+window.title("Snake game") #عنوان الشاشه
+window.resizable(False, False)#تغيير حجم الشاشه 
+
+score = 0 #النتيجة
+direction = 'down' # انتجاه حركه الثعبان اول ماتبدأ اللعبه 
+
+label = Label(window, text="Score:{}".format(score), font=('consolas', 40))
+label.pack() 
+
+canvas = Canvas(window, bg=BACKGROUND_COLOR, height=GAME_HEIGHT, width=GAME_WIDTH)#اعدادات لوحه اللعبة
+canvas.pack()
 
 window.update()
-
-
-
-window_width = window.winfo_width()
-window_height =window.winfo_height()
+# winfo يعرض المعلومات الخاصه باللوح من طول وعرض والشاشه
+window_width = window.winfo_width() 
+window_height = window.winfo_height()
 screen_width = window.winfo_screenwidth()
-screen_height =window.winfo_screenheight()
+screen_height = window.winfo_screenheight()
+# احداثيات اللعبة الشاشه على 2 وكمان طولها وعرضها بحيث ان شاشه العرض تكون ف النص
+x = int((screen_width/2) - (window_width/2))
+y = int((screen_height/2) - (window_height/2))
 
+window.geometry(f"{window_width}x{window_height}+{x}+{y}") # تحدد ابعاد الشاشه
+# علشان تغير وتتحكم باتجاه الثعبان change_direction تعرف الازرار من الكيبورد وتقرأ ما ضغط عليه المستخدم وتستدعي داله
+window.bind('<Left>', lambda event: change_direction('left'))
+window.bind('<Right>', lambda event: change_direction('right'))
+window.bind('<Up>', lambda event: change_direction('up'))
+window.bind('<Down>', lambda event: change_direction('down'))
 
+snake = Snake()
+food = Food()
 
-x=int(screen_width/2)-(window_width/2)
-y=int(screen_height/2)-(window_height/2)
-window.geometry(f"{window_width} x {window_height}+{x}+{y}")
-snake=snake()
-food = food()
-window.mainloop()
+next_turn(snake, food)# اتدعاء داله next_turn
 
-window.mainloop()
+window.mainloop()#  تشغيل الحلقه الرئيسية للعبه وقررأة النقر ع الازرار 
